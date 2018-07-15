143cae6b9d30413c89ed32a2796b28d51388fd1f
LaraLaPierre <larablapierre@gmail.com>
2018-07-15 18:25:05 -0500

    refactored validation for game setup input for human vs. human game play

--- validate_user_setup.rb
+++ validate_user_setup.rb
@@ -1,14 +1,19 @@
 class ValidateUserSetup
 
   def validate_name(user_input, names_chosen)
-    if !names_chosen.include?(user_input)
-      if user_input.length > 1 && !is_numeric?(user_input)
-        if !user_input.include? " "
-          valid_input = true
-        else 
-          valid_input = false
-        end 
+
+    if user_input != nil
+      if !names_chosen.include?(user_input)
+        if user_input.length > 1 && !is_numeric?(user_input)
+          if !user_input.include? " "
+            valid_input = true
+          else 
+            valid_input = false
+          end 
+        end
       end
+    else
+      valid_input = false
     end     
   end 
 
@@ -22,12 +27,7 @@ class ValidateUserSetup
         end 
       end
     end
-  end 
-
-  def validate_player_sequence(user_input)
-
-    
-  end 
+  end  
 
   def is_numeric?(user_input)
     !!Float(user_input) rescue false
